6dad2f5aee6346aa0d45eb57085050b9d5059341
Simon Baslé <sbasle@pivotal.io>
2017-03-13 10:03:46 +0100

    prevents null reqtask executor, shutdown it at dispose time

--- src/main/java/reactor/core/publisher/TopicProcessor.java
+++ src/main/java/reactor/core/publisher/TopicProcessor.java
@@ -16,6 +16,7 @@
 
 package reactor.core.publisher;
 
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadFactory;
@@ -681,6 +682,8 @@ public final class TopicProcessor<E> extends EventLoopProcessor<E>  {
 			return signal;
 		}, waitStrategy);
 
+		Objects.requireNonNull(requestTaskExecutor, "requestTaskExecutor");
+
 		this.minimum = RingBuffer.newSequence(-1);
 		this.barrier = ringBuffer.newReader();
 		this.requestTaskExecutor = requestTaskExecutor;
@@ -780,6 +783,11 @@ public final class TopicProcessor<E> extends EventLoopProcessor<E>  {
 		if (!alive() && SUBSCRIBER_COUNT.get(TopicProcessor.this) == 0) {
 			WaitStrategy.alert();
 		}
+	}
+
+	@Override
+	protected void specificShutdown() {
+		requestTaskExecutor.shutdown();
 	}
 
 	/**
--- src/main/java/reactor/core/publisher/WorkQueueProcessor.java
+++ src/main/java/reactor/core/publisher/WorkQueueProcessor.java
@@ -16,6 +16,7 @@
 
 package reactor.core.publisher;
 
+import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
@@ -597,6 +598,8 @@ public final class WorkQueueProcessor<E> extends EventLoopProcessor<E> {
 				FACTORY,
 				waitStrategy);
 
+		Objects.requireNonNull(requestTaskExecutor, "requestTaskExecutor");
+
 		this.writeWait = waitStrategy;
 
 		ringBuffer.addGatingSequence(workSequence);
@@ -705,6 +708,11 @@ public final class WorkQueueProcessor<E> extends EventLoopProcessor<E> {
 		if (!alive()) {
 			WaitStrategy.alert();
 		}
+	}
+
+	@Override
+	protected void specificShutdown() {
+		requestTaskExecutor.shutdown();
 	}
 
 	/**
