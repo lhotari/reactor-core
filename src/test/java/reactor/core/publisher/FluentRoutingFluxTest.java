7757ae1dd467d6db195906958594d997b8eb1f3e
Lari Hotari <lari@hotari.net>
2017-04-05 22:56:55 +0300

    Add failing tests

--- src/test/java/reactor/core/publisher/FluentRoutingFluxTest.java
+++ src/test/java/reactor/core/publisher/FluentRoutingFluxTest.java
@@ -1,5 +1,6 @@
 package reactor.core.publisher;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import reactor.util.concurrent.QueueSupplier;
 
@@ -24,6 +25,37 @@ public class FluentRoutingFluxTest {
         Mono<List<Integer>> oddListMono = oddFlux.collectList().subscribe();
 
         assertEquals(Arrays.asList(2, 4), evenListMono.block());
+        assertEquals(Arrays.asList(1, 3, 5), oddListMono.block());
+    }
+
+    @Ignore
+    @Test
+    public void fluentRoutingSubscribePartiallyLastUnconsumed() {
+        FluentRoutingFlux<Integer, Integer> routingFlux = FluentRoutingFlux.create(Flux.range(1, 5),
+                QueueSupplier.SMALL_BUFFER_SIZE, QueueSupplier.get(QueueSupplier.SMALL_BUFFER_SIZE), value -> value);
+
+        Flux<Integer> evenFlux = routingFlux.route(x -> x % 2 == 0);
+        routingFlux.route(x -> x % 2 != 0); // this is unused to test partially subscribed downstream fan-out
+
+        routingFlux.connect();
+
+        MonoProcessor<List<Integer>> evenListMono = evenFlux.collectList().subscribe();
+
+        assertEquals(Arrays.asList(2, 4), evenListMono.block());
+    }
+
+    @Test
+    public void fluentRoutingSubscribePartiallyLastConsumed() {
+        FluentRoutingFlux<Integer, Integer> routingFlux = FluentRoutingFlux.create(Flux.range(1, 5),
+                QueueSupplier.SMALL_BUFFER_SIZE, QueueSupplier.get(QueueSupplier.SMALL_BUFFER_SIZE), value -> value);
+
+        routingFlux.route(x -> x % 2 == 0);
+        Flux<Integer> oddFlux = routingFlux.route(x -> x % 2 != 0).log();
+
+        routingFlux.connect();
+
+        Mono<List<Integer>> oddListMono = oddFlux.collectList().subscribe();
+
         assertEquals(Arrays.asList(1, 3, 5), oddListMono.block());
     }
 
